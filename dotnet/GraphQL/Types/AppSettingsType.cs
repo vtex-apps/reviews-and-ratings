ed579d27b504c07bb5882bffcb71c7d366d66b86
Steven Bowen <steven.bowen@vtex.com.br>
2020-09-23 10:07:22 -0400

    Add open reviews configuration

--- dotnet/GraphQL/Types/AppSettingsType.cs
+++ dotnet/GraphQL/Types/AppSettingsType.cs
@@ -16,6 +16,7 @@ namespace ReviewsRatings.GraphQL.Types
             Field(b => b.RequireApproval).Description("Indicates whether reviews require approval.");
             Field(b => b.UseLocation).Description("Indicates whether to use Location field.");
             Field(b => b.defaultOpen).Description("Indicates whether reviews should expand by default");
+            Field(b => b.defaultOpenCount).Description("Indicates number of reviews to be expanded by default");
         }
     }
 }
