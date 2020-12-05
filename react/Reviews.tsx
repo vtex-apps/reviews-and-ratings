81fc042352a4378ac06d540960104c17cfcf7d67
khrizzcristian <43498488+khrizzcristian@users.noreply.github.com>
2020-12-05 18:05:12 +0200

    Update react/Reviews.tsx
    Co-authored-by: Breno Calazans <brenocalazans@gmail.com>

--- react/Reviews.tsx
+++ react/Reviews.tsx
@@ -493,7 +493,6 @@ const Reviews: FunctionComponent<InjectedIntlProps & Props> = props => {
         const graphArray = [0, 0, 0, 0, 0, 0]
         graphArray[0] = total
         if (reviews) {
-          // eslint-disable-next-line array-callback-return
           reviews.map((review: Review) => {
             const thisRating = review.rating
             graphArray[thisRating] += 1
