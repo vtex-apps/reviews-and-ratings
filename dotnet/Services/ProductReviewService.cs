355ab564da3dc04545c55ec3638bb9756b47d0dd
Brian Talma <47258865+btalma@users.noreply.github.com>
2021-01-22 10:25:08 -0500

    Update ProductReviewService.cs

--- dotnet/Services/ProductReviewService.cs
+++ dotnet/Services/ProductReviewService.cs
@@ -46,7 +46,6 @@
                 else
                 {
                     retval = false;
-                    Console.WriteLine($"    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Unknown Product Id for {id}");
                 }
 
                 // also remove the reference to the review from the loopup
@@ -94,8 +93,6 @@
                 {
                     decimal totalRating = reviews.Sum(r => r.Rating);
                     averageRating = totalRating / numberOfReviews;
-
-                    //Console.WriteLine($" >>>>>>>>>>>>>>>>>>>>>>>>>>>> Average Rating for {productId}: {averageRating} from {numberOfReviews} reviews.");
                 }
             }
 
@@ -139,7 +136,6 @@
             if (to > 0)
             {
                 take = Math.Min((to - from) + 1, maximumReturnedRecords);
-                //Console.WriteLine($"    >>>>>>>>>>>>>>>>>  Take {take} reviews {from}-{to}");
             }
 
             reviews = reviews.Skip(from - 1).Take(take).ToList();
@@ -162,10 +158,8 @@
                     }.Any(s => s.Contains(searchTerm))).ToList();
                 }
 
-                //Console.WriteLine($"    >>>>>>>>>>>>>>>>>   {reviews.Count} reviews (unfiltered)");
                 if (!string.IsNullOrEmpty(orderBy))
                 {
-                    Console.WriteLine($"    >>>>>>>>>>>>>>>>>   Order By {orderBy}");
                     string[] orderByArray = orderBy.Split(DELIMITER);
                     PropertyInfo pi = typeof(Review).GetProperty(orderByArray[0]);
                     if (pi != null)
@@ -212,15 +206,10 @@
                             }
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine($"    >>>>>>>>>>>>>>>>>   Could not get {orderBy} property info.");
-                    }
                 }
 
                 if (!string.IsNullOrEmpty(status))
                 {
-                    //Console.WriteLine($"------------||| Returning order with Approved status of {status} ||||----------------");
                     reviews = reviews.Where(x => x.Approved.Equals(Boolean.Parse(status))).ToList();
                 }
             }
@@ -250,14 +239,11 @@
             }
 
             limit = Math.Min(limit, maximumReturnedRecords);
-            //Console.WriteLine($"    >>>>>>>>>>>>>>>>>  GetReviewsByProductId: {productId}  offset:{offset} limit:{limit} orderBy:{orderBy}");
             IList<Review> reviews = await this._productReviewRepository.GetProductReviewsAsync(productId);
             if (reviews != null && reviews.Count > 0)
             {
-                //Console.WriteLine($"    >>>>>>>>>>>>>>>>>   {reviews.Count} reviews for {productId} (unfiltered)");
                 if (!string.IsNullOrEmpty(orderBy))
                 {
-                    Console.WriteLine($"    >>>>>>>>>>>>>>>>>   Order By {orderBy}");
                     string[] orderByArray = orderBy.Split(DELIMITER);
                     PropertyInfo pi = typeof(Review).GetProperty(orderByArray[0]);
                     if (pi != null)
@@ -280,14 +266,9 @@
                             reviews = reviews.OrderBy(x => pi.GetValue(x, null)).ToList();
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine($"    >>>>>>>>>>>>>>>>>   Could not get {orderBy} property info.");
-                    }
                 }
 
                 reviews = reviews.Skip(offset).Take(limit).ToList();
-                Console.WriteLine($"    >>>>>>>>>>>>>>>>>   {reviews.Count} reviews for {productId} (filtered)");
             }
             else
             {
@@ -304,8 +285,6 @@
             {
                 IDictionary<int, string> lookup = await _productReviewRepository.LoadLookupAsync();
 
-                // Console.WriteLine($"    >>>>>>>>>>>>>>>>>   ID:{review.Id}  Exists?{lookup.ContainsKey(review.Id)}");
-
                 int maxKeyValue = 0;
                 if (lookup != null && lookup.Count > 0)
                 {
@@ -318,22 +297,12 @@
 
                 review.Id = ++maxKeyValue;
                 review.CacheId = review.Id;
-                //if(string.IsNullOrWhiteSpace(review.ReviewerName))
-                //{
-                //    review.ReviewerName = string.Empty;
-                //    //review.ReviewerName = "anon";
-                //}
 
                 if (string.IsNullOrWhiteSpace(review.ReviewDateTime))
                 {
                     // TODO: Check timezone for store
                     review.ReviewDateTime = DateTime.Now.ToString();
                 }
-
-                //if (string.IsNullOrWhiteSpace(review.Sku))
-                //{
-                //    review.Sku = string.Empty;
-                //}
 
                 string productId = review.ProductId;
 
@@ -344,17 +313,9 @@
                 }
 
                 reviews.Add(review);
-
-                Console.WriteLine($"    >>>>>>>>>>>>>>>>>   Saving [{review.Id}] {productId}");
-
                 await this._productReviewRepository.SaveProductReviewsAsync(productId, reviews);
-
                 lookup.Add(review.Id, review.ProductId);
                 await this._productReviewRepository.SaveLookupAsync(lookup);
-            }
-            else
-            {
-                Console.WriteLine("-!-!- New Review - Null review.");
             }
 
             return review;
@@ -362,34 +323,25 @@
 
         public async Task<IList<Review>> GetReviewsByShopperId(string shopperId)
         {
-            Console.WriteLine($"    >>>>>>>>>>>>>>>>>   GetReviewsByShopperId {shopperId}");
-            //IList<Review> reviews = await this.GetReviews(0, maximumReturnedRecords, string.Empty);
             IList<Review> reviews = await this.GetReviews();
-            Console.WriteLine($"    >>>>>>>>>>>>>>>>>   GetReviewsByShopperId {shopperId} - {reviews.Count} total reviews");
             reviews = reviews.Where(r => r.ShopperId == shopperId).ToList();
-            Console.WriteLine($"    >>>>>>>>>>>>>>>>>   {reviews.Count} reviews for {shopperId}");
 
             return reviews;
         }
 
         public async Task ClearData()
         {
-            Console.WriteLine("------------------------------------------------------------------------");
             IDictionary<int, string> lookup = await _productReviewRepository.LoadLookupAsync();
             if (lookup != null)
             {
                 List<string> productIds = lookup.Values.Distinct().ToList();
-                Console.WriteLine($"    >>>>>>>>>>>>>>>>>>>>>>  productIds {productIds.Count}");
                 foreach (string productId in productIds)
                 {
-                    Console.WriteLine($"    >>>>>>>>>>>>>>>>>>>>>>  Deleting reviews for {productId}");
                     await this._productReviewRepository.SaveProductReviewsAsync(productId, null);
                 }
             }
 
-            Console.WriteLine("     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  Deleting lookup.");
             await _productReviewRepository.SaveLookupAsync(null);
-            Console.WriteLine("------------------------------------------------------------------------");
         }
 
         private async Task<string> LookupProductById(int Id)
@@ -432,7 +384,6 @@
                 }
                 else
                 {
-                    Console.WriteLine($"    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Unknown Product Id for {id}");
                     retval = false;
                 }
             }
@@ -480,7 +431,6 @@
             {
                 VtexOrder vtexOrder = await this._productReviewRepository.GetOrderInformation(orderId);
                 var productIds = vtexOrder.Items.Select(i => i.ProductId);
-                //Console.WriteLine($"productIds = [ {JsonConvert.SerializeObject(productIds)} ]");
                 hasPurchased = productIds.Contains(productId);
                 if(hasPurchased)
                 {
