8b07cc7855be29e6bed7409f2a5e6d67d130f832
Brian Talma <47258865+btalma@users.noreply.github.com>
2021-01-22 10:21:17 -0500

    Update ProductReviewRepository.cs

--- dotnet/DataSources/ProductReviewRepository.cs
+++ dotnet/DataSources/ProductReviewRepository.cs
@@ -54,8 +54,6 @@
 
         public async Task<IList<Review>> GetProductReviewsAsync(string productId)
         {
-            Console.WriteLine($"GetProductReview called with {this._httpContextAccessor.HttpContext.Request.Headers[HEADER_VTEX_ACCOUNT]},{this._httpContextAccessor.HttpContext.Request.Headers[HEADER_VTEX_WORKSPACE]},{this._environmentVariableProvider.ApplicationName},{this._environmentVariableProvider.ApplicationVendor},{this._environmentVariableProvider.Region}");
-
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
@@ -78,10 +76,6 @@
             }
 
             response.EnsureSuccessStatusCode();
-
-            // responseContent = Utils.ReviewSanitizer.SantizeReviewText(responseContent);
-
-            //Console.WriteLine($"Before DeserializeObject");
             IList<Review> productReviews = null;
             try
             {
@@ -92,15 +86,12 @@
                 Console.WriteLine($"DeserializeObject Error: {ex.Message} ");
                 Console.WriteLine($"{{\"__VTEX_IO_LOG\":true, \"service\":\"review\", \"error\":\"{ex.Message}\", \"productId\":\"{productId}\"}}");
             }
-
-            //Console.WriteLine($"productReviews.Count = {productReviews.Count}");
+            
             return productReviews;
         }
 
         public async Task SaveProductReviewsAsync(string productId, IList<Review> productReviews)
         {
-            Console.WriteLine($"        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SaveProductReviewsAsync for [{productId}]");
-
             if (productReviews == null)
             {
                 productReviews = new List<Review>();
@@ -124,8 +115,6 @@
             var response = await client.SendAsync(request);
 
             response.EnsureSuccessStatusCode();
-
-            // Console.WriteLine($"        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {response.IsSuccessStatusCode} {response.ReasonPhrase}");
         }
 
         public async Task<IDictionary<int, string>> LoadLookupAsync()
@@ -210,7 +199,6 @@
             var client = _clientFactory.CreateClient();
             var response = await client.SendAsync(request);
             string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"VALIDATE USER: [{response.StatusCode}] responseContent = {responseContent}");
             if (response.IsSuccessStatusCode)
             {
                 validatedUser = JsonConvert.DeserializeObject<ValidatedUser>(responseContent);
@@ -246,20 +234,12 @@
             var client = _clientFactory.CreateClient();
             var response = await client.SendAsync(request);
             string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"ValidateKeyAndToken: [{response.StatusCode}] responseContent = {responseContent}");
 
             return response.IsSuccessStatusCode;
         }
 
         public async Task<VtexOrder> GetOrderInformation(string orderId)
         {
-            //Console.WriteLine("------- Headers -------");
-            //foreach (var header in this._httpContextAccessor.HttpContext.Request.Headers)
-            //{
-            //    Console.WriteLine($"{header.Key}: {header.Value}");
-            //}
-            //Console.WriteLine($"http://{this._httpContextAccessor.HttpContext.Request.Headers[Constants.VTEX_ACCOUNT_HEADER_NAME]}.{Constants.ENVIRONMENT}.com.br/api/checkout/pvt/orders/{orderId}");
-
             VtexOrder vtexOrder = null;
 
             try
@@ -271,18 +251,13 @@
                 };
 
                 request.Headers.Add(USE_HTTPS_HEADER_NAME, "true");
-                //request.Headers.Add(Constants.ACCEPT, Constants.APPLICATION_JSON);
-                //request.Headers.Add(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
                 string authToken = this._httpContextAccessor.HttpContext.Request.Headers[HEADER_VTEX_CREDENTIAL];
-                //Console.WriteLine($"Token = '{authToken}'");
                 if (authToken != null)
                 {
                     request.Headers.Add(AUTHORIZATION_HEADER_NAME, authToken);
                     request.Headers.Add(VTEX_ID_HEADER_NAME, authToken);
                     request.Headers.Add(PROXY_AUTHORIZATION_HEADER_NAME, authToken);
                 }
-
-                //StringBuilder sb = new StringBuilder();
 
                 var client = _clientFactory.CreateClient();
                 var response = await client.SendAsync(request);
@@ -293,14 +268,10 @@
                     vtexOrder = JsonConvert.DeserializeObject<VtexOrder>(responseContent);
                     Console.WriteLine($"GetOrderInformation: [{response.StatusCode}] ");
                 }
-                else
-                {
-                    Console.WriteLine($"GetOrderInformation: [{response.StatusCode}] '{responseContent}'");
-                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"GetOrderInformation Error: {ex.Message}");
+                
             }
 
             return vtexOrder;
@@ -318,19 +289,13 @@
                     RequestUri = new Uri($"http://{this._httpContextAccessor.HttpContext.Request.Headers[VTEX_ACCOUNT_HEADER_NAME]}.{ENVIRONMENT}.com.br/api/oms/pvt/orders?{queryString}")
                 };
 
-                //request.Headers.Add(USE_HTTPS_HEADER_NAME, "true");
-                //request.Headers.Add(Constants.ACCEPT, Constants.APPLICATION_JSON);
-                //request.Headers.Add(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
                 string authToken = this._httpContextAccessor.HttpContext.Request.Headers[HEADER_VTEX_CREDENTIAL];
-                //Console.WriteLine($"Token = '{authToken}'");
                 if (authToken != null)
                 {
                     request.Headers.Add(AUTHORIZATION_HEADER_NAME, authToken);
                     request.Headers.Add(VTEX_ID_HEADER_NAME, authToken);
                     request.Headers.Add(PROXY_AUTHORIZATION_HEADER_NAME, authToken);
                 }
-
-                //StringBuilder sb = new StringBuilder();
 
                 var client = _clientFactory.CreateClient();
                 var response = await client.SendAsync(request);
@@ -348,7 +313,7 @@
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"ListOrders Error: {ex.Message}");
+                
             }
 
             return vtexOrderList;
