2893c1dbe02183be1ad754e6097410d18d0fed7f
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2021-03-12 14:04:42 +0200

    no message

---
+++ dotnet/GraphQL/Types/TotalsType.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using ReviewsRatings.Models;
+using ReviewsRatings.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewsRatings.GraphQL.Types
+{
+    [GraphQLMetadata("Totals")]
+    public class TotalsType : ObjectGraphType<Totals>
+    {
+        public TotalsType(IProductReviewService productReviewService)
+        {
+            Name = "Range";
+            Field(b => b.Total5);
+            Field(b => b.Total4);
+            Field(b => b.Total3);
+            Field(b => b.Total2);
+            Field(b => b.Total1);
+        }
+    }
+}
