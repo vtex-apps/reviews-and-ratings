0f198c2bbb2a7154db08a0085ca6297bffbc27dc
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-05-04 12:26:46 +0530

    Removed approveReviews from UI testcase + enable sendDashboard for all testcases

--- cypress/integration/2.2.3-signed_in_user.spec.js
+++ cypress/integration/2.2.3-signed_in_user.spec.js
@@ -4,7 +4,6 @@ import {
   preserveCookie,
 } from '../support/common/support.js'
 import { testCase2 } from '../support/review_and_ratings.outputvalidation'
-import { approveReviews } from '../support/graphql_testcase.js'
 
 const { title, configuration, product, user1 } = testCase2
 
@@ -19,10 +18,6 @@ describe(`${title} - Add review in ordered product`, () => {
       cy.addReview(product, configuration.defaultStarsRating, user1)
     }
   )
-
-  // If we disable admin approval, review been shown to UI
-  // but review status is still pending so approving it via graphql
-  approveReviews(user1.name)
 
   preserveCookie()
 })
