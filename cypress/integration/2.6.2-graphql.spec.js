a597f990c278e930fe82fb5cf00a7edf14e832cb
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-05-04 20:41:01 +0530

    Graphql testcase 2.6.2 fix

--- cypress/integration/2.6.2-graphql.spec.js
+++ cypress/integration/2.6.2-graphql.spec.js
@@ -5,16 +5,19 @@ import {
 import { testSetup, updateRetry } from '../support/common/support.js'
 import { testCase6 } from '../support/review_and_ratings.outputvalidation.js'
 
-const { anonymousUser1, anonymousUser2 } = testCase6
-const { productId } = anonymousUser1
+describe('Graphql queries', () => {
+  const { anonymousUser1, anonymousUser2 } = testCase6
+  const { productId } = anonymousUser1
 
-it('Verify get average of product by id query', updateRetry(4), () => {
   testSetup(false)
-  cy.addDelayBetweenRetries(2000)
-  graphql(getAverageRatingByProductId(productId), response => {
-    expect(response.body.data.averageRatingByProductId).to.not.equal(null)
-    expect(response.body.data.averageRatingByProductId).to.equal(
-      anonymousUser2.rating
-    )
+
+  it('Verify get average of product by id query', updateRetry(4), () => {
+    cy.addDelayBetweenRetries(2000)
+    graphql(getAverageRatingByProductId(productId), response => {
+      expect(response.body.data.averageRatingByProductId).to.not.equal(null)
+      expect(response.body.data.averageRatingByProductId).to.equal(
+        anonymousUser2.rating
+      )
+    })
   })
 })
